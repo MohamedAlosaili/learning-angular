2e877b56d119dc745d696ebe3b5f223810ba1588
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-08-09 01:35:17 +0300

    Remove unnecessary import statements from card and product components

--- mo-store/src/app/Home/card/card.component.ts
+++ mo-store/src/app/Home/card/card.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { Product } from 'src/app/types';
-import { Subscription } from 'rxjs';
 import { NumberOfItems } from 'src/app/base/NumberOfItems.component';
 
 @Component({
--- mo-store/src/app/product/product.component.ts
+++ mo-store/src/app/product/product.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../types';
 import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../services/cart.service';
-import { Subscription } from 'rxjs';
 import { NumberOfItems } from '../base/NumberOfItems.component';
 
 @Component({
