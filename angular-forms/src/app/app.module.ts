86dfb15768369854e97dcb91638c164dc56dd054
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-08-12 08:35:12 +0300

    Add FormsModule and UserSettingComponent to app.module.ts

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { UserSettingComponent } from './user-setting/user-setting.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UserSettingComponent],
+
+  // To work with forms (e.g. form Directives) we need FormsModule + Provide Angular form features
+  // Now, Angular aware of our forms
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
