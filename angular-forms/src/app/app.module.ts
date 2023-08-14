5ed08e5319b9df7d4283ff2fe0995408e9138e07
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-08-14 08:43:53 +0300

    Add routes and import HttpClientModule in app.module.ts

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { UserSettingComponent } from './user-setting/user-setting.component';
+import { SubmittedComponent } from './submitted/submitted.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserSettingComponent,
+  },
+  { path: 'submitted', component: SubmittedComponent },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, UserSettingComponent],
+  declarations: [AppComponent, UserSettingComponent, SubmittedComponent],
 
   // To work with forms (e.g. form Directives) we need FormsModule + Provide Angular form features
   // Now, Angular aware of our forms
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
