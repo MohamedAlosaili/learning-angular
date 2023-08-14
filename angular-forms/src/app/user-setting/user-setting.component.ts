d97cd00e3d1d9b20d847adaa7d4db593a9cd705c
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-08-14 08:46:32 +0300

    Add data service and form submission handling

--- angular-forms/src/app/user-setting/user-setting.component.ts
+++ angular-forms/src/app/user-setting/user-setting.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from './data/user-settings';
+import { DataService } from './data/data.service';
+import { Observable, Subscription, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-user-setting',
@@ -12,12 +15,16 @@ export class UserSettingComponent {
    * In this feature, we make the user focus on important fields and shows other fields when the important is filled
    */
   nameValid = false;
+  postSub: Subscription | undefined;
+  submitErrorMessage?: string;
+  loading = false;
+  countries$!: Observable<string[]>;
 
   /**
    * Copying form data:
    */
   originalUserSettings: UserSettings = {
-    name: '',
+    name: 'Salem',
     emailOffer: false,
     theme: 'system',
     country: 'saudi arabia',
@@ -28,13 +35,42 @@ export class UserSettingComponent {
   // ⚠ Spread copy the top-level property, if you need to get a deep copy: https://developer.mozilla.org/en-US/docs/Glossary/Deep_copy
   userSettings: UserSettings = { ...this.originalUserSettings };
 
+  constructor(private data: DataService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.countries$ = this.data.getFormCountries();
+  }
+
   onSubmit(form: NgForm) {
-    // Validation before submitting the form to the server
+    this.submitErrorMessage = undefined;
+    // Validate before submitting the form to the server
     console.log(`Form is ${form.valid ? 'valid' : 'invalid'}`);
-    console.log(form);
+    console.log(form.valid);
+
+    if (form.valid) {
+      this.loading = true;
+      this.postSub = this.data.postUserSettings(this.userSettings).subscribe({
+        next: (data) => {
+          this.loading = false;
+          console.log(data);
+          this.router.navigate(['/submitted']);
+        },
+        error: this.handleSubmitError.bind(this),
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.postSub?.unsubscribe();
   }
 
   onBlur(name: NgModel) {
     this.nameValid = !!name.valid;
+  }
+
+  handleSubmitError(err: any) {
+    this.loading = false;
+    this.submitErrorMessage =
+      'Failed to submit the form, please try again later.';
   }
 }
